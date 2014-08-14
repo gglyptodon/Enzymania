d5af001fbca1f141aaeb7fb2183ddd5ad4bf5fd6
frogsicle <a7denton@gmail.com>
2014-08-14 21:30:29 +0200

    added score, 1 pathway at a time, and scheming

---
+++ enzymania/ali_thinking.py
@@ -0,0 +1,11 @@
+# while reading xml file. Keep list of all products and all reactants. (dictionary)
+
+# throw out everything that is not both reactant and product (not in a circle)
+
+# while there are still keys in dictionary:
+	# take value as key for next until initial returns (count)
+
+# whichever list is longest
+# choose random int between 0 and len(count) - 1 start there. End at int -1
+
+
